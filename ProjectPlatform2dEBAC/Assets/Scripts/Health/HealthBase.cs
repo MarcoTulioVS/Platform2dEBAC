add500dc9a5958865d7e20b004f2ff8abffb3ca7
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-02-19 12:36:12 -0300

    Adding health control
    Prototype scene

---
+++ ProjectPlatform2dEBAC/Assets/Scripts/Health/HealthBase.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    public int startLife = 10;
+
+    private int _currentLife;
+
+    private bool _isDead;
+
+    public bool destroyOnKill;
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _currentLife = startLife;
+        _isDead = false;
+    }
+
+    public void Damage(int damage)
+    {
+        if (!_isDead)
+        {
+            _currentLife -= damage;
+
+            if (_currentLife <= 0)
+            {
+                Kill();
+            }
+        }
+       
+    }
+
+    private void Kill()
+    {
+        _isDead = true;
+
+        if (destroyOnKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
